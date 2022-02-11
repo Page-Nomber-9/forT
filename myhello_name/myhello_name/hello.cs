a5c647e174b3c35bed3d5ab5e601000060616362
Page-Nomber-9 <99385527+Page-Nomber-9@users.noreply.github.com>
2022-02-11 10:20:08 +0300

    Add files via upload

---
+++ myhello_name/myhello_name/hello.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace myhello
+{
+    class hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            string a = Convert.ToString(args[0]);
+            if (a == "myhello")
+            {
+                string b = Convert.ToString(args[1]);
+                Console.WriteLine($"привет, {b}");
+            }
+            else
+            {
+               Console.WriteLine("твоё имя... НАЗАВИ МНЕ СВОЁ ИМЯ!");
+                    string b = Convert.ToString(args[1]);
+                    Console.WriteLine($"привет, {b}");
+               
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ myhello_name/mysum/sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace mysum
+{
+    class sum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+            double a = Convert.ToDouble(args[0]);
+            double b = Convert.ToDouble(args[1]);
+            Console.WriteLine(a+b);
+            Console.ReadKey();
+        }
+    }
+}
