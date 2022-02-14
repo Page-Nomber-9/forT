7afc3e8ab924335e90eb91bd85bd3335b58e8164
Page-Nomber-9 <99385527+Page-Nomber-9@users.noreply.github.com>
2022-02-14 11:42:20 +0300

    Add files via upload

---
+++ Chisla/ArMax/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ArMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b=1, c=1, i=0;
+            bool f = false;
+            for (int n = 0; f==false; n++)
+            {
+                a = Convert.ToInt32(
+                if (a>=b)
+                {
+                    i = n;
+                    c = b;
+                    b = a;
+                }
+            }
+            Console.WriteLine($"{c}[{i}");
+        }
+    }
+}
---
+++ Chisla/Chisla/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Chisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1, y = 1;
+            Console.WriteLine(args.Length);
+            int a = Convert.ToInt32(args[0]);
+            int b = Convert.ToInt32(args[1]);
+            while (a/10>0)
+            {
+                a = a / 10;
+                x++;
+            }
+            while (b / 10 > 0)
+            {
+                b = b / 10;
+                y++;
+            }
+            Console.WriteLine($"в 1-м числе {x} цифр. В 2-м числе {y} цифр");
+        }
+    }
+}
