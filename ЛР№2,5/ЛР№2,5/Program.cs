4705c650bbb60df4c0991ffccfc1a3661e9348f9
Page-Nomber-9 <99385527+Page-Nomber-9@users.noreply.github.com>
2022-02-16 10:54:30 +0300

    Add files via upload

--- ЛР№2,5/ЛР№2,5/Program.cs
+++ ЛР№2,5/ЛР№2,5/Program.cs
@@ -9,6 +9,27 @@ namespace ЛР_2_5
             /* найти разность между максимальным и минимальным элементами массива.
              * поменять местами макс и мин массива
              * отсортировать массив*/
+            void ArMax(in int [] AAA, in int K, out int Mx, out int Mn, out int MxI, out int MnI)
+            {
+                Mx = AAA[0];
+                Mn = AAA[0];
+                MxI = 0;
+                MnI = 0;
+                for (int i=0; i<K; i++)
+                {
+                    if (AAA[i] > Mx)
+                    {
+                        MxI = i;
+                        Mx = AAA[i];
+                    }
+                    //Нашёл минимальный элемент и его индекс
+                    if (AAA[i] < Mn)
+                    {
+                        MnI = i;
+                        Mn = AAA[i];
+                    }
+                }
+            }
             Console.WriteLine("Введите количество элементов массива");
             int n = Convert.ToInt16(Console.ReadLine());
             int[] ar = new int [n];
@@ -20,34 +41,34 @@ namespace ЛР_2_5
             int Max = ar[0];
             int Min = Max;
             int IMin = 0, IMax = 0;
-            for (int i = 0; i < n; i++)
+            ArMax(ar, n, out Max, out Min, out IMax, out IMin);
+            Console.WriteLine($"максимальный элемент массива: {Max} с индексом {IMax}.\nМинимальный элемент массива: {Min} с индексом {IMin}");
+            Console.WriteLine("Я всё перемешал и теперь массив выглядит так:");
+            ar[IMin] = Max;
+            ar[IMax] = Min;
+            bool P = true;
+            //для возможности подмены элементов в массиве
+            int copy = 0;
+            //Пузырьковая сортировка: концепция 2
+            for (int i=0; i<n-1; i++)
             {
-                //Нашём максимальный элемент и его индекс
-                if (ar[i] > Max)
+                if (P==false)
                 {
-                    IMax = i;
-                    Max = ar[i];
+                    P = true;
+                    i = 0;
                 }
-                //Нашёл минимальный элемент и его индекс
-                if (ar[i] < Min)
+                if (ar[i] > ar[i + 1])
                 {
-                    IMin = i;
-                    Min = ar[i];
+                    P = false;
+                    copy = ar[i];
+                    ar[i] = ar[i + 1];
+                    ar[i + 1] = copy;
+                    i = 0;
                 }
             }
-            Console.WriteLine($"Максимальный элемент: {Max}/ Минимальный элемент массива: {Min}\nИх разность равна: {Max-Min}");
-            //Поменял их местами
-            ar[IMax] = Min; ar[IMin] = Max;
             for (int i = 0; i < n; i++)
             {
-                Console.Write($"{ar[i]} ");
-            }
-            //Сортируем, сортируем!
-            Array.Sort(ar);
-            Console.WriteLine("\n*********");
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"{ar[i]} ");
+                Console.WriteLine(ar[i]);
             }
             Console.ReadKey();
         }
