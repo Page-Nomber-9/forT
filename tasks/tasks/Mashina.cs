b4a5a0511845d72fe44d625d6c64d6fe02690e5b
Page-Nomber-9 <99385527+Page-Nomber-9@users.noreply.github.com>
2022-02-18 09:58:15 +0300

    Add files via upload

---
+++ tasks/tasks/Mashina.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tasks
+{
+    class Mashina
+    {
+        List<string> nomb = new List<string>() {"К999МА", "Е005АМ", "В802ОР"};
+        List<string> mark = new List<string>() {"Ford", "Nissan", "Toyota"};
+        List<string> model = new List<string>() {"Focus", "Almera", "Camry"};
+        public void Start(in string x)
+        {
+        Mashina mash = new Mashina();       
+        int i = mash.model.IndexOf(x);
+        Console.WriteLine($"Машина {mash.mark[i]} {mash.model[i]} завелась ;)");
+                
+            
+        }
+        public void Stop(in string x)
+        {
+         Mashina mash = new Mashina();
+        int i = mash.model.IndexOf(x);
+         Console.WriteLine($"Машина {mash.mark[i]} {mash.model[i]} остановилась ;)");
+        }
+    }
+}
---
+++ tasks/tasks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool forever = true;
+            Console.WriteLine("делай что хочешь! если не знаешь как - введи help");
+            while (forever==true)
+            {
+                string a = Console.ReadLine();
+                if (a=="end")
+                {
+                    forever = false;
+                }
+                if (a == "help")
+                {
+                    Console.WriteLine("stop - останавливает машину определённой марки");
+                    Console.WriteLine("start - заводит машину определённой марки");
+                    Console.WriteLine("end - завершить программу");
+                    Console.WriteLine("help - выводит команды и их функции");
+                }
+                if (a == "start")
+                {
+                    string x = Console.ReadLine();
+                    Mashina M = new Mashina();
+                    M.Start(x);
+                }
+                if (a == "stop")
+                {
+                    string x = Console.ReadLine();
+                    Mashina M = new Mashina();
+                    M.Stop(x);
+                }
+            }
+        }
+    }
+}
