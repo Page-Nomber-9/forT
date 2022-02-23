bc3b1af961e631ec50a197a3318f5936ac2bd6cc
Page-Nomber-9 <99385527+Page-Nomber-9@users.noreply.github.com>
2022-02-23 21:37:20 +0300

    Add files via upload

---
+++ Apteka/Apteka/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Apteka
+{
+    class Ap
+    {
+        List<string> namele = new List<String>() {"Ламизил", "Амброксол", "йодомарин", "Панангин", "Финлепсин"};
+        //моя программа, что хочу то ворочу
+        string nameap="АХАХАХАХАХАХАХАХАХАХ";
+        List<int> cost = new List<int>() {390, 40, 256, 170, 242};
+        public void Showinf()
+        {
+            Console.WriteLine($"в компании {nameap} есть такие припараты:");
+            Ap a = new Ap();
+            for (int i=0; i<a.cost.Count;i++)
+            {
+                Console.WriteLine($"{a.namele[i]}({a.cost[i]}руб.), ");
+            }
+        }
+        public void Maxcost()
+        {
+            Ap a = new Ap();
+            int k = 0;
+            int m = a.cost[0];
+            for (int i=0; i < a.cost.Count; i++)
+            {
+                if (m<a.cost[i])
+                {
+                    m = a.cost[i];
+                    k = i;
+                }
+            }
+            Console.WriteLine($"самый дорогой припарат - {m}({k}руб.)");
+        }
+        public void Allcost()
+        {
+            Ap a = new Ap();
+            int m = 0;
+            for (int i = 0; i < a.cost.Count; i++)
+            {
+                m = m + a.cost[i];
+            }
+            Console.WriteLine($"общая стоимость всех припаратов - {m}руб.");
+        }
+    }
+
+
+}
---
+++ Apteka/Apteka/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Apteka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет!");
+            Console.WriteLine("введите команыды (если вы не знаете команд, то введите 'help'");
+            bool forever = true;
+            //хоть тут и forever, программа не бесконечна
+            while (forever == true)
+            {
+                string comand = Console.ReadLine();
+                if (comand=="end")
+                {
+                    forever = false;
+                }
+                if (comand=="inf")
+                {
+                    Ap sh = new Ap();
+                    sh.Showinf();
+                    sh.Maxcost();
+                    sh.Allcost();
+                }
+                if (comand == "max")
+                {
+                    Ap sh = new Ap();
+                    sh.Maxcost();
+                }
+                if (comand == "all")
+                {
+                    Ap sh = new Ap();
+                    sh.Allcost();
+                }
+                if (comand == "help")
+                {
+                    Console.WriteLine("end - позволяет завершить работу программы");
+                    Console.WriteLine("all - выводит общую стоимость припаратов");
+                    Console.WriteLine("max - выводит название и стоимость самого дорогого припарата");
+                    Console.WriteLine("inf - выводит всю информацию");
+                    Console.WriteLine("help - выводит список команд и их функции");
+                }
+            }
+        }
+    }
+}
